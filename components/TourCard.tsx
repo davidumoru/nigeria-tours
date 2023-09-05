f9edc2acd97e3c8709bd92abb57d14549fd9d21b
David Umoru <umorudavido@gmail.com>
2023-09-05 17:43:09 +0100

    Display first 100 char and adjust button style

--- components/TourCard.tsx
+++ components/TourCard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 interface TourCardProps {
   image: string;
@@ -8,7 +8,13 @@ interface TourCardProps {
   onRemove: () => void;
 }
 
-const TourCard: React.FC<TourCardProps> = ({ image, price, name, description, onRemove }) => {
+const TourCard: React.FC<TourCardProps> = ({
+  image,
+  price,
+  name,
+  description,
+  onRemove,
+}) => {
   const [showDescription, setShowDescription] = useState(false);
 
   const toggleDescription = () => {
@@ -19,26 +25,33 @@ const TourCard: React.FC<TourCardProps> = ({ image, price, name, description, on
     onRemove();
   };
 
+  const displayDescription = showDescription
+    ? description
+    : description.slice(0, 100); // Display the first 100 characters initially
+
   return (
-    <div className="border border-gray-200 p-4 rounded-lg shadow-lg mb-4">
+    <div className="border border-gray-200 p-4 rounded-lg shadow-md mb-4">
       <img src={image} alt={name} className="mb-2" />
       <div className="flex justify-between items-center mb-2">
         <span className="text-xl font-semibold">{name}</span>
         <span className="text-gray-500">{price}</span>
       </div>
-      <p className={showDescription ? 'block' : 'hidden'}>{description}</p>
+      <p className="mb-2">{displayDescription}</p>
+
       <button
         onClick={toggleDescription}
         className="text-blue-500 hover:underline focus:outline-none"
       >
-        {showDescription ? 'Show Less' : 'Show More'}
+        {showDescription ? "Show Less" : "Read More"}
       </button>
-      <button
-        onClick={handleNotInterested}
-        className="mt-2 text-red-500 hover:underline focus:outline-none"
-      >
-        Not Interested
-      </button>
+      <div className="flex flex-col">
+        <button
+          onClick={handleNotInterested}
+          className="text-red-500 hover:underline focus:outline-none border border-red-500 mt-2 px-4 py-1 rounded-md"
+        >
+          Not Interested
+        </button>
+      </div>
     </div>
   );
 };
