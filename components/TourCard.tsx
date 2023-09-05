96502d2f3f62f3e95ce3703fa0d839fefb067f87
David Umoru <umorudavido@gmail.com>
2023-09-05 04:39:15 +0100

    Create tour card component

---
+++ components/TourCard.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+interface TourCardProps {
+  image: string;
+  price: string;
+  name: string;
+  description: string;
+}
+
+const TourCard: React.FC<TourCardProps> = ({ image, price, name, description }) => {
+  const [showDescription, setShowDescription] = useState(false);
+
+  const toggleDescription = () => {
+    setShowDescription(!showDescription);
+  };
+
+  const handleNotInterested = () => {
+    
+  };
+
+  return (
+    <div className="border border-gray-200 p-4 rounded-lg shadow-lg mb-4">
+      <img src={image} alt={name} className="mb-2" />
+      <div className="flex justify-between items-center mb-2">
+        <span className="text-xl font-semibold">{name}</span>
+        <span className="text-gray-500">{price}</span>
+      </div>
+      <p className={showDescription ? 'block' : 'hidden'}>{description}</p>
+      <button
+        onClick={toggleDescription}
+        className="text-blue-500 hover:underline focus:outline-none"
+      >
+        {showDescription ? 'Show Less' : 'Show More'}
+      </button>
+      <button
+        onClick={handleNotInterested}
+        className="mt-2 text-red-500 hover:underline focus:outline-none"
+      >
+        Not Interested
+      </button>
+    </div>
+  );
+};
+
+export default TourCard;
