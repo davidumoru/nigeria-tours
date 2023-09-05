3b1741956753a049694b757c18b44507a7049b2e
David Umoru <umorudavido@gmail.com>
2023-09-05 17:13:30 +0100

    Adjust body styling

--- pages/nigeria-tours.tsx
+++ pages/nigeria-tours.tsx
@@ -59,8 +59,8 @@ const NigeriaTours: React.FC = () => {
 
   return (
     <div>
-      <h1 className="text-3xl font-semibold mb-4">Nigeria Tours</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <h1 className="text-3xl font-semibold my-10 text-center">Nigeria Tours</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mx-10 md:mx-32">
         {tours.map((tour, index) => (
           <TourCard key={index} {...tour} onRemove={() => handleRemoveTour(index)} />
         ))}
