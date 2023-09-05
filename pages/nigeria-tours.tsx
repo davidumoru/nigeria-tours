ae0b848904d875da10666a44a9d6e9d140c477e9
David Umoru <umorudavido@gmail.com>
2023-09-05 16:46:37 +0100

    Add image path

--- pages/nigeria-tours.tsx
+++ pages/nigeria-tours.tsx
@@ -4,49 +4,49 @@ import TourCard from "../components/TourCard";
 const NigeriaTours: React.FC = () => {
   const [tours, setTours] = useState([
     {
-      image: "/tour1.jpg",
+      image: "/Olumo-Rock.jpeg",
       price: "$100",
       name: "Tour 1",
       description: "Description for Tour 1...",
     },
     {
-      image: "/tour2.jpg",
+      image: "/kajuru11.jpeg",
       price: "$150",
       name: "Tour 2",
       description: "Description for Tour 2...",
     },
     {
-      image: "/tour3.jpg",
+      image: "/nike4.jpeg",
       price: "$120",
       name: "Tour 3",
       description: "Description for Tour 3...",
     },
     {
-      image: "/tour4.jpg",
+      image: "/Obudu-Ranch.jpeg",
       price: "$90",
       name: "Tour 4",
       description: "Description for Tour 4...",
     },
     {
-      image: "/tour5.jpg",
+      image: "/Osun-sacred-grove.jpeg",
       price: "$200",
       name: "Tour 5",
       description: "Description for Tour 5...",
     },
     {
-      image: "/tour6.jpg",
+      image: "/zuma-rock.jpeg",
       price: "$80",
       name: "Tour 6",
       description: "Description for Tour 6...",
     },
     {
-      image: "/tour7.jpg",
+      image: "/lekki-conservation.jpeg",
       price: "$130",
       name: "Tour 7",
       description: "Description for Tour 7...",
     },
     {
-      image: "/tour8.jpg",
+      image: "/eko-beach.jpeg",
       price: "$110",
       name: "Tour 8",
       description: "Description for Tour 8...",
